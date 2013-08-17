2c7c05b6edf997e85037ff2320c042139c1cd6f7
Chen Ling <chen.ling.1@stonybrook.edu>
2013-08-17 00:56:35 -0400

    commit hello world

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		// TODO Auto-generated method stub
+
+	}
+
+}
